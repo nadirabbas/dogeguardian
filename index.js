f11750736e9a94bdcdf93faf2d944b883f50a790
Nadir Abbas <nadirabbasforwork@gmail.com>
2021-03-15 11:22:13 +0500

    add sandbox mode

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-require('dotenv').config();
-const puppeteer = require('puppeteer');
-const app = require('express')();
+require("dotenv").config();
+const puppeteer = require("puppeteer");
+const app = require("express")();
 
 const credentials = {
-    token: process.env.ACCESS_TOKEN,
-    refresh: process.env.REFRESH_TOKEN
+  token: process.env.ACCESS_TOKEN,
+  refresh: process.env.REFRESH_TOKEN,
 };
 
 (async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: ['--headless']
-    })
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      "--headless",
+      "--ignore-certificate-errors",
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+    ],
+  });
 
-    const page = await browser.newPage()
-    await page.goto("https://dogehouse.tv")
-    await page.waitForSelector("button")
-    await page.evaluate(credentials => {
-        localStorage.setItem("@toum/token", credentials.token)
-        localStorage.setItem("@toum/refresh-token", credentials.refresh)
-    }, credentials)
-    await page.goto(process.env.ROOM_LINK)
+  const page = await browser.newPage();
+  await page.goto("https://dogehouse.tv");
+  await page.waitForSelector("button");
+  await page.evaluate(credentials => {
+    localStorage.setItem("@toum/token", credentials.token);
+    localStorage.setItem("@toum/refresh-token", credentials.refresh);
+  }, credentials);
+  await page.goto(process.env.ROOM_LINK);
 
+  app.get("/refresh", async (_, res) => {
+    await page.reload();
+    res.send("i refreshed");
+  });
+})();
 
-    app.get('/refresh', async (_, res) => {
-        await page.reload();
-        res.send('i refreshed')
-    })
-})()
-
-const port = process.env.PORT || 5678
-app.listen(port, '0.0.0.0', () => console.log('dude, I am LIVE! at ' + port))
+const port = process.env.PORT || 5678;
+app.listen(port, "0.0.0.0", () => console.log("dude, I am LIVE! at " + port));
